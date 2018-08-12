650b488e4614e3f49c8a97502b7bf079e5b49621
grandiloquent <1785907865@qq.com>
2018-08-13 06:59:31 +0800

    changed

--- othres/Tools v2/README.md
+++
@@ -1,8 +0,0 @@
-# Kotlin Tools
-
-- [x] 分割本地视频
-- [x] 播放本地视频
-- [x] 播放本地音乐
-- [x] 浏览本地图片
-- [x] 下载网络文件
-- [x] 大量 Kotlin For Android 实例
--- othres/Tools v2/app/src/main/java/psycho/euphoria/common/extension/Drawable.kt
+++
@@ -1,6 +0,0 @@
-package psycho.euphoria.common.extension
-
-import android.graphics.PorterDuff
-import android.graphics.drawable.Drawable
-
-fun Drawable.applyColorFilter(color: Int) = mutate().setColorFilter(color, PorterDuff.Mode.SRC_IN)
