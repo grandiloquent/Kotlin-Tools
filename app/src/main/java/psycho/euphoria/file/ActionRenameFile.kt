9828284ebf17614eabae1c6b4a62bebd94680fd1
grandiloquent <1785907865@qq.com>
2018-08-25 02:04:07 +0800

    changed

--- app/src/main/java/psycho/euphoria/file/ActionRenameFile.kt
+++ app/src/main/java/psycho/euphoria/file/ActionRenameFile.kt
@@ -1,2 +1,18 @@
 package psycho.euphoria.file
 
+import android.content.Context
+import android.os.Build
+import android.provider.DocumentsContract
+import psycho.euphoria.common.Services
+import psycho.euphoria.common.extension.needsStupidWritePermissions
+import java.io.File
+
+private fun renameFile(context: Context, oldFile: File, newFile: File) {
+    if (context.needsStupidWritePermissions(oldFile.absolutePath)) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            DocumentsContract.renameDocument(context.contentResolver, generateDocumentUriFast(oldFile, Services.treeUri), newFile.name)
+        }
+    } else {
+        oldFile.renameTo(newFile)
+    }
+}
